3b64651e046cb0ec23cf869e4e776bbf94047cd0
Ashish Kumar Singh <Ashish Kumar Singh@users.noreply.seal.dev>
2026-03-18 00:37:29 +0000

    fix: move backend selector to global segmented control, enable SMP for all images

--- src/index.ts
+++ src/index.ts
@@ -292,7 +292,7 @@ export default {
           description: def.description,
           drive: def.drive,
           memory: def.memory,
-          backends: key === "aqeous" ? ["v86", "v86-smp"] : ["v86"],
+          backends: ["v86", "v86-smp"],
         })),
       );
     }
